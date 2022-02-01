27413281d37e2c3fc246e571b7e98cf5e3c5e58b
piyi-juanjis <94360229+piyi-juanjis@users.noreply.github.com>
2022-02-01 10:33:10 +0100

    Update Clasificacion.java

--- src/Funciones/Clasificacion.java
+++ src/Funciones/Clasificacion.java
@@ -1,5 +1,34 @@
 package Funciones;
 
 public class Clasificacion {
+	Liga liga;
+	EquipoClasificacion[] equipos;
+
+	public Liga getLiga() {
+		return liga;
+	}
+	public void setLiga(Liga liga) {
+		this.liga = liga;
+	}
+	public EquipoClasificacion[] getEquipos() {
+
+		//Este es el metodo que calcula la clasificacion 
+		return equipos;
+	}
+
+	public void mostrarClasificacion() {
+
+	}
+
+
+	public void setEquipos(Equipo[] equipos) {
+		this.equipos = new EquipoClasificacion[equipos.length];
+		//creo un EquipoClasificacion por cada Equipo de la liga
+		for(int i=0; i<equipos.length;i++) {
+			this.equipos[i].setEquipo(equipos[i]);
+		}
+	}
+
+
 
 }
