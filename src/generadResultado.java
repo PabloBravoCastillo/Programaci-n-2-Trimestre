fdc631ce330060319c6192cc53aaf1816a96a354
piyi-juanjis <94360229+piyi-juanjis@users.noreply.github.com>
2022-02-01 09:51:55 +0100

    p

--- src/generadResultado.java
+++ src/generadResultado.java
@@ -1,40 +1,25 @@
-package Funciones;
+import java.util.Scanner;
 
-public class Jornadas {
-	
-		 
-
-
-        // TODO Auto-generated method stub
-        Scanner teclado=new Scanner(System.in);
+public class generadResultado {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el nombre del equipo 1");
         String equipo1=teclado.nextLine();
         System.out.println("Introduce el nombre del equipo 2");
         String equipo2=teclado.nextLine();
- 
-
-       
-        int partido = 0;
+		int partido = 0;
         int resultado = 0;
 
         while (partido<1) {
 
             int goles1 = (int) (Math.random()*10+1);
             int goles2 = (int) (Math.random()*10+1);
-
-           
-
-
-
-        }
+            
             System.out.println("Resultados del partido "+goles1+"-"+ goles2);
             partido++;
-        }
 
-            
+	}
+        
+    }
 }
-	
-	
-	
-
-
