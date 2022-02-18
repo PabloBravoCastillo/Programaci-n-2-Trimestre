d670071fd25c534fb112e8f9bf829e880d483772
FranHL2003 <francisco.s235304@cesurformacion.com>
2022-02-18 21:03:51 +0100

    cambios

--- src/Funciones/Calendario.java
+++ src/Funciones/Calendario.java
@@ -21,17 +21,93 @@ public class Calendario {
 			emparejamientos[1][j]=equipos[numeroEquipos-1-j];
 		}
 		for(int i=0; i<numeroJornadas/2; i++) {
-			Partido[]partidosIda =new Partido
+			Partido[]partidosIda =new Partido[numeroPartidos];
+			Partido[]partidosVuelta=new Partido[numeroPartidos];
 			
 			
-			
-			
+			for(int j=0; j<numeroPartidos;j++) {
 			//Partidos de ida
-			partidosIda[j].setLocal(emparejamientos[0][j]);
+			partidosIda[j].setEquipoLocal(emparejamientos[0][j]);
+			partidosIda[j].setEquipoVisitante(emparejamientos[i][j]);
+		    //partidos de vuelta
+			partidosVuelta[j].setEquipoLocal(emparejamientos[1][j]);
+			partidosVuelta[j].setEquipoVisitante(emparejamientos[0][j]);
 			
-		}
+			}
+			//ACTUALIZAMOS JORNADAS
+			jornadas[i].setPartidos(partidosIda);
+			jornadas[numeroJornadas-1-i].setPartidos(partidosVuelta);
+			
+			//rotamos el array doble
+			Equipo[][]auxEmp = new Equipo[2][numeroPartidos];
+			//actualizamos columna izquierda
+			for(int k=0;k<numeroPartidos;k++) {
+				if(k==0) {
+					auxEmp[0][k]=emparejamientos[0][k];
+				}else if(k>0 && k<numeroPartidos-1) {
+					auxEmp[0][k+1]=emparejamientos[0][k];
+					}else {
+						auxEmp[1][k]=emparejamientos[0][k];
+					}
+					}
+			 //Columna Derecha
+			for(int k=0;k<numeroPartidos;k++) {
+				if(k>0 && k<numeroPartidos-1) {
+					auxEmp[1][k-1]=emparejamientos[1][k];
+				}else {
+					auxEmp[0][1]=emparejamientos[1][k];
+				}
+			}
+				}
+	
+			for(int i=0; i<numeroJornadas/2; i++) {
+				
+				Partido[]partidosIda=new Partido[numeroPartidos];
+				Partido[]partidosVuelta=new Partido[numeroPartidos];
+				
+				for(int j=0; j<numeroPartidos;j++) {
+					//partidos de ida
+					PartidosIda[j].setEquipoLocal(emparejamientos[0][j]);
+					PartidosIda[j].setEquipoVisitante(emparejamientos[1][j]);
+					PartidosIda[j].setArbitro(arbitros[j]);
+					//partidos de vuelta
+					
+					PartidosVuelta[j].setEquipoLocal(emparejamientos[1][j]);
+					PartidosVuelta[j].setEquipoVisitante(emparejamientos[0][j]);
+					PartidosVuelta[j].setArbitro(arbitros[j]);
+				}
+				//actualizamos jornadas
+				jornadas[i]=new Jornada();
+				jornadas[i].setPartidos(partidosIda);
+				jornadas[numeroJornadas-1-i].setPartidos(partidosVuelta);
+			}
+			//Rotamos el array doble
+			Equipo[][]auxEmp=new Equipo[2][numeroPartidos];
+			//actualizamos columna izquierda
+			for(int k=0;k<numeroPartidos;k++) {
+				if(k==0) {
+					auxEmp[0][k]=emparejamientos[0][k];
+				}else if(k>0 && k<numeroPartidos-1) {
+					auxEmp[0][k+1]=emparejamientos[0][k];
+					}else {
+						auxEmp[1][k]=emparejamientos[0][k];
+					}
+					}
+			 //Columna Derecha
+			for(int k=0;k<numeroPartidos;k++) {
+				if(k>0 && k<numeroPartidos-1) {
+					auxEmp[1][k-1]=emparejamientos[1][k];
+				}else {
+					auxEmp[0][1]=emparejamientos[1][k];
+				}
+			}
+				}
+	
+			
+				
+			
+			
 		
-	}
 
 		//Para sacar las jornadas necesito traerme todos los equipos
 		
@@ -53,3 +129,4 @@ public class Calendario {
 	}
 
 }
+	
